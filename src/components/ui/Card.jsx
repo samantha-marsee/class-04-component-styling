7e9cfff9f22a453bb7acc524453ef2b8691fe1fe
samantha-marsee <sam572@cornell.edu>
2025-02-03 16:23:17 +0000

    updated

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -2,7 +2,7 @@ import Citation from "../Citation";
 
 export default function Card() {
   return (
-    <div>
+    <div className = "card">
       <img src="/public/images/galaxy.webp" alt="an AI generated galaxy" />
 
       <Citation />
